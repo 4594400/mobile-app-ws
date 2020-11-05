cf6d16393879fab704e940a6486c3afbc46dd14d
anDrekon <4594400@gmail.com>
2020-11-05 18:52:22 +0200

    3. Spring Security: login by Json web token (JWT)

---
+++ src/main/java/com/doc/mobileappws/secutity/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.doc.mobileappws.secutity;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 864000000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/users";
+    public static final String TOKEN_SECRET = "jh43kj3434kfgdfgsdfrhgsdfsdfgdfsardfgfdgsdfsdafhghj4r4563453454565fjttytre54565retk5k3b";
+}
