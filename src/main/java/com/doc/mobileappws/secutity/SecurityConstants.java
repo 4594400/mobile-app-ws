b93f77cbc02943e41502a8a2427c75f637373f93
anDrekon <4594400@gmail.com>
2020-11-09 15:03:29 +0200

    3. Spring Security: Reading Token Secret from a property file

--- src/main/java/com/doc/mobileappws/secutity/SecurityConstants.java
+++ src/main/java/com/doc/mobileappws/secutity/SecurityConstants.java
@@ -1,9 +1,15 @@
 package com.doc.mobileappws.secutity;
 
+import com.doc.mobileappws.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "jh43kj3434kfgdfgsdfrhgsdfsdfgdfsardfgfdgsdfsdafhghj4r4563453454565fjttytre54565retk5k3b";
+
+    public static String getTokenSecret(){
+        ApplicationProperties applicationProperties = (ApplicationProperties) SpringApplicationContext.getBean("applicationProperties");
+        return applicationProperties.getTokenSecret();
+    }
 }
