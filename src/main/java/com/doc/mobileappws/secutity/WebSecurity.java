e73b7db2974b138d214c1753d34a51fe83c17db0
anDrekon <4594400@gmail.com>
2020-11-09 14:27:44 +0200

    3. Spring Security: Implementing Authorization Filter

--- src/main/java/com/doc/mobileappws/secutity/WebSecurity.java
+++ src/main/java/com/doc/mobileappws/secutity/WebSecurity.java
@@ -27,7 +27,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
         .and()
-        .addFilter(getAuthenticationFilter());
+        .addFilter(getAuthenticationFilter())
+        .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
     @Override
