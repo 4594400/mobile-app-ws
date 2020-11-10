736906299d2faeb859eeb001c48cb63d7f2bc325
anDrekon <4594400@gmail.com>
2020-11-10 15:19:15 +0200

    10. Deploying application (Generating WAR file)

--- src/main/java/com/doc/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/doc/mobileappws/MobileAppWsApplication.java
@@ -2,11 +2,13 @@ package com.doc.mobileappws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MobileAppWsApplication.class, args);
@@ -22,4 +24,13 @@ public class MobileAppWsApplication {
         return new SpringApplicationContext();
     }
 
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }*/
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MobileAppWsApplication.class);
+    }
 }
